beede32917ddbd6eed43225ef2d05b9385811b9a
Student1 Git <student1.git@gmail.com>
2016-04-19 14:20:05 -0500

    commit.

---
+++ TestProject/src/com/example/main/TestClass.java
@@ -0,0 +1,10 @@
+package com.example.main;
+
+public class TestClass {
+	
+	public static void main(String args[]) {
+		System.out.println("Test Project");
+		System.out.println("Test Project - commit");
+	}
+
+}
